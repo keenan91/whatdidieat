bc8a8394d296cd41b67f3e4ddca373d941b07688
Keenan Matrix <keenanmatrixpt@gmail.com>
2021-01-22 18:42:11 -0500

    Testing connection from query to UI

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>{}</h1>
+        <h1 className={styles.title}>{first}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
