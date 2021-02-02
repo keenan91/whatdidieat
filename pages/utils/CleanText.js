1f62182d4401e9ddb7bd387b790696fc1233184e
Keenan Matrix <keenanmatrixpt@gmail.com>
2021-02-02 11:58:02 -0500

    Correcting Error

--- pages/utils/CleanText.js
+++ pages/utils/CleanText.js
@@ -4,14 +4,17 @@ const CleanText = ({text}) => {
     ?.replace(/\//g, '')
     ?.replace(/%/g, '')
     ?.toLowerCase()
-  const words = stringChanged ? stringChanged.split(' ') : ' '
-
-  for (let i = 0; i < words?.length; i++) {
-    if (words[i][0] == undefined) {
-    } else {
-      words[i] = words[i][0].toUpperCase() + words[i].substr(1) + ' '
+  const words = stringChanged ? stringChanged.split(' ') : undefined
+  if (words == undefined) {
+  } else {
+    for (let i = 0; i < words?.length; i++) {
+      if (words[i][0] == undefined) {
+      } else {
+        words[i] = words[i][0].toUpperCase() + words[i].substr(1) + ' '
+      }
     }
   }
+
   return words
 }
 
